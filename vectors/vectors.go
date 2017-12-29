7c67b43c43ed1e1433cd56fbbbdee93778bc562c
Starz0r <starz0rdesign@gmail.com>
2017-12-29 02:47:13 -0600

    Added Vectors Package which include a new data structure

---
+++ vectors/vectors.go
@@ -0,0 +1,46 @@
+package vectors
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Vector struct {
+	slice  reflect.Value
+	typeof reflect.Type
+}
+
+func NewVector(z interface{}) *Vector {
+	t := reflect.TypeOf(z)
+
+	return &Vector{
+		slice:  reflect.MakeSlice(reflect.SliceOf(t), 0, 0),
+		typeof: t,
+	}
+}
+
+func newVector(t reflect.Type, len, cap int) *Vector {
+	return &Vector{
+		slice:  reflect.MakeSlice(reflect.SliceOf(t), len, cap),
+		typeof: t,
+	}
+}
+
+func (v *Vector) Get(index int) interface{} {
+	return v.slice.Index(index)
+}
+
+func (v *Vector) Put(element interface{}) {
+
+	if reflect.ValueOf(element).Type() != v.slice.Type().Elem() {
+		panic(fmt.Sprintf("Put: cannot put a %T into a slice of %s", element, v.slice.Type().Elem()))
+	}
+
+	v.slice = reflect.Append(v.slice, reflect.ValueOf(element))
+}
+
+func (v *Vector) Copy() *Vector {
+	v2 := newVector(v.typeof, v.slice.Len(), v.slice.Cap())
+	reflect.Copy(v2.slice, v.slice)
+	return v2
+}
